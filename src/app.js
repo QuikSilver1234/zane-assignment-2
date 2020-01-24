b9f59b78e18f578cb58c493041df9be76bf62700
Zane Paton <zanerepaton@gmail.com>
2020-01-24 11:03:54 -0600

    iniial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from "./header";
+
+export class App extends Component {
+    render() {
+        return (
+            <div className="header">
+                <Header />
+            </div>
+
+        )
+    }
+}
+
+export default App
---
+++ src/header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <h1>Popular League of Legends Marksmen</h1>
+            </div>
+        )
+    }
+}
+
+export default Header
